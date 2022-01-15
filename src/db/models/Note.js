25e478f915af5e08fefed39c5b93af9ee395ffb0
Bogdan-Zinovij <bogdanolexandrov@gmail.com>
2022-01-15 16:59:21 +0200

    created model for note table

---
+++ src/db/models/Note.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const DataTypes = require('sequelize');
+const db = require('../db');
+
+const Note = db.define(
+  'note',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    folderId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING(45),
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING(8000),
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  },
+  {
+    db,
+    timestamps: false,
+    freezeTableName: true,
+  }
+);
+
+module.exports = Note;
