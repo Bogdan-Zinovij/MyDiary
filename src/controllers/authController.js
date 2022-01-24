b5756467bb27b0a95fac92af35b7015a2465410d
Bogdan-Zinovij <bogdanolexandrov@gmail.com>
2022-01-24 12:59:53 +0200

    added registration validation

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,9 +1,17 @@
+'use strict';
+
 const User = require('../db/models/User');
 const generateAccessToken = require('../utils/generateAccessToken');
+const { validationResult } = require('express-validator');
 
 class AuthController {
   async registration(req, res) {
     try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ message: errors });
+      }
+
       await User.create(req.body);
 
       res.status(201).json({ message: 'User was created!' });
--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -2,9 +2,10 @@ const Router = require('express');
 const authRouter = new Router();
 const authController = require('../controllers/authController');
 const authMiddleware = require('../middleware/authMiddleware');
+const registrValidationScheme = require('../validation/registrValidation');
 
 authRouter
-  .post('/registration', authController.registration)
+  .post('/registration', registrValidationScheme, authController.registration)
   .post('/login', authController.login)
   .get('/users', authMiddleware, authController.getUsers);
 
